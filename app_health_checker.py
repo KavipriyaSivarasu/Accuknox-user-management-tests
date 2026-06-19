34a3f9b925410166f334cabdb06ff4199c2aa174
KavipriyaSivarasu <kavipriyasivarasu04@gmail.com>
2026-06-19 16:49:53 +0530

    Add health checker for application status

---
+++ app_health_checker.py
@@ -0,0 +1,10 @@
+import requests
+url = input("Enter URL: ")
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("Application is UP")
+    else:
+        print("Application is DOWN")
+except:
+    print("Application is Unreachable")
